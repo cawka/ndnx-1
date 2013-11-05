05e8f070b4ffa5a8fa45f4096e63f50b3781b2de
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-11-04 17:27:06 -0800

    Initial import of debian/* script for making binary distribution

---
+++ debian/ndnx.default.ex
@@ -0,0 +1,10 @@
+# Defaults for ndnx initscript
+# sourced by /etc/init.d/ndnx
+# installed at /etc/default/ndnx by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libndnx 0.2~rc1 ndnx (>> 0.2~rc1-0), ndnx (<< 0.2~rc1-99)
